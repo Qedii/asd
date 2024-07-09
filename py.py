66598a0068b367dfb00592474043a4b05f544bb8
Ismayil Zayidov <zaidov033@gmail.com>
2024-07-09 14:52:43 +0400

    ---------------------

--- py.py
+++ py.py
@@ -19,7 +19,6 @@ while True:
                 print(" * ", end="")
             print()
 
-
     elif input1 == 1:
         num = int(input("enter the number of rows: "))
         for i in range(1,num+1):
