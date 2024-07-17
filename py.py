1d2ba890a0cc7b3d151d259a5f6a922b3a7ece64
Ismayil Zayidov <zaidov033@gmail.com>
2024-07-17 14:21:00 +0400

    ---------------------

--- py.py
+++ py.py
@@ -1,12 +1,14 @@
 f = open("locked_loans.txt")
 
 locked_ids = f.readlines()
+locked_ids.__contains__(locked_ids)
 
 print(locked_ids)
 
 x = open("waiver_0.txt")
 
 file_ids = x.readlines()
+file_ids.__contains__()
 
 print(file_ids)
 
